287697d6c5cc8ad80a9b748ef95eadcc89bcaf2c
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-01 16:26:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,7 +7,7 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-def catch_phrase
+def catch_phrase(phrase)
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
